d65eff4bb69331c545da3c67f0865c2d3778571b
Demis Rincon <darmfma@gmail.com>
2023-12-09 22:48:20 -0600

    first commit

--- models/guests.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const guestSchema = new mongoose.Schema({
-    guests: [String]
-});
-
-const Guest = mongoose.model('Guest', guestSchema);
-
-module.exports = Guest;
